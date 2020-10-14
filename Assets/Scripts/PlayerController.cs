8bf12444b4ddd9005241228cf1bf264faf85cfb4
Joel Ring <joelwofo@users.noreply.github.com>
2020-10-14 14:44:03 +0200

    Enabled player jump
    Player jump Involded some fine trimming of different colliders.
    We don't want player to be able to jump when sidse of body is colliding with platform.
    Therefore feet were added and checked if feet only are in contact with ground or platform.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,19 +8,25 @@ using System.Collections.Generic;
 using UnityEngine;public class PlayerController : MonoBehaviour
 {
     [Header("References")]
-    public Transform PlayerTransform;
+    public Transform playerTransform;
 
 
-    [Header("Game Behaviour")] public float PlayerSpeed = 2f;
+    [Header("Game Behaviour")] 
+    public float playerSpeed = 2f;
+    public float jumpStrength = 30f;
+    public float jumpDisableDelay = 0.02f;
     
-    //player specifics
+    //player components
     private Vector3 _spawnPosition;
     private Rigidbody2D _playerRB;
     
+    //player mechanics
+    private bool jumpIsPossible = false;
+    
     void Start()
     {
-        _spawnPosition = PlayerTransform.transform.position;
-        _playerRB = PlayerTransform.GetComponent<Rigidbody2D>();
+        _spawnPosition = playerTransform.transform.position;
+        _playerRB = playerTransform.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -33,14 +39,48 @@ using UnityEngine;public class PlayerController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            Debug.Log("moving left");
-            _playerRB.AddForce(Vector2.left * PlayerSpeed );
+            _playerRB.AddForce(Vector2.left * playerSpeed );
             
         }
+        
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            Debug.Log("moving right");
-            _playerRB.AddForce(Vector2.right * PlayerSpeed );
+            _playerRB.AddForce(Vector2.right * playerSpeed );
         }
+
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            if (jumpIsPossible)
+            {
+                Debug.Log("jumping");
+                _playerRB.AddForce(Vector2.up * jumpStrength);
+            }
+
+        }
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        
+        if ( (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Platform")) && 
+             (collision.otherCollider.name == "RFoot") || (collision.otherCollider.name == "LFoot"))
+        {
+            jumpIsPossible = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        Debug.Log("exit collision");
+        if (other.gameObject.CompareTag("Ground") || other.gameObject.CompareTag("Platform"))
+        {
+            //if disable immediately, the jump will be too low
+            Invoke("DisableJump", jumpDisableDelay);
+        }
+    }
+
+    private void DisableJump()
+    {
+        jumpIsPossible = false;
     }
 }
